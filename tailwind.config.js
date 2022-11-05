0fd526e6485ac9ea9871599ecf77ab2738102fd6
Tanmay Chakrabarty <tanmay23c@gmail.com>
2022-11-05 10:10:58 +0530

    🐱‍🏍 final code commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"], //add the classes from the content html and js files
   theme: {
     extend: {
-      fontsize : {
+      fontSize : {
         sm : ["14px"], //add own font size to the tailwind library
     },
+    colors: {
+      starGreen: '#D4E9E2',
+      darkGreen: "#1E3932",
+      lightText: "rgba(0,0,0, .58)",
+      titleText: "rgba(0,0,0, .87)",
   },
 },
   plugins: [],
+}
 }
